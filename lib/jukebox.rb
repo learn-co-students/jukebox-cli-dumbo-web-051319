f838c4c9260cd9c5d258d6fbcb16d7b793595b40
Yonas Fesehatsion <yone920@gmail.com>
2019-05-04 20:23:13 -0400

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,4 @@
+require 'pry'
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -10,11 +11,53 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
-def say_hello(name)
-  "Hi #{name}!"
+def help
+puts  "I accept the following commands:"
+puts  "- help : displays this help message"
+puts  "- list : displays a list of songs you can play"
+puts  "- play : lets you choose a song to play"
+puts  "- exit : exits this program"
 end
- 
-puts "Enter your name:"
-users_name = gets.chomp
- 
-puts say_hello(users_name)
+
+def list(songs)
+  songs.each_with_index do |song, number| 
+    # binding.pry
+    puts "#{number + 1}. #{song}"
+  end
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  answer = gets.chomp
+  songs.each_with_index do |song, number|
+    number = number + 1
+     if answer == song  
+       puts "Playing #{song}"
+     elsif answer.to_i == number
+      puts "Playing #{song}"
+     else
+      puts "Invalid input, please try again"
+     end
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  puts "Please enter a command:"
+  answer = gets.chomp
+  if answer == "list"
+    list(songs)
+  elsif answer == "play"
+    play(songs)
+  elsif answer == "help"
+    help
+  elsif answer == "exit"
+    exit
+  end
+  
+
+end
