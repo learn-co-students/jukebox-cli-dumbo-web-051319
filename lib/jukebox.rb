f2c82f1889f89847f3e7d06b61219034529579b3
github username <github email address>
2019-05-11 16:09:52 -0400

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,56 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  #this method should be the same as in jukebox.rb
+puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
+end
+
+def list(songs)
+songs.each.with_index do |name, num|
+    puts "#{num}. #{name}"
+  end
+end
+
+def play(songs)
+  puts "enter your number or name: "
+  input = gets.chomp
+  songs.each.with_index(1) do |name, num|
+    if input.to_i == num
+      puts "#{name}"
+    elsif input == name
+      puts "#{name}"
+    else
+      puts "Invalid input, please try again"
+end
+end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+  exit
+end
+
+def run(songs)
+command =""
+while command
+puts "Please enter a command: "
+command = gets.chomp
+case command
+  when "exit"
+    exit_jukebox
+  when  "list"
+    list(songs)
+  when  "play"
+    play(songs)
+  when  "help"
+    help
+    break
+  else help
+  end
+end
+end
